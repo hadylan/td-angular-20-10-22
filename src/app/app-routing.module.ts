81d8fdffeeca791c2b8df05cd3f2992ebe6c4d0c
YohanNzi <yohannzi.pro@gmail.com>
2022-11-15 13:37:25 +0100

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './book-store/book-list/book-list.component'; 
 
 const routes: Routes = [];
 
