3aeae3e501515b9afcd48aea4ad24f9f754e1c89
YohanNzi <yohannzi.pro@gmail.com>
2022-11-15 13:51:09 +0100

    pushmain

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookListComponent } from './book-store/book-list/book-list.component'; 
+import { BookListComponent } from './book-store/book-list/book-list.component';
+import { BookDetailComponent } from './book-store/book-detail/book-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'book/edit/:name', component: BookDetailComponent },
+  { path: 'book/list', component: BookListComponent },
+  { path: '', redirectTo: '/book/list', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
