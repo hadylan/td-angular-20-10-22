6cad4ad18624702ab9d8cf3f098dcf9ba67db6fa
YohanNzi <yohannzi.pro@gmail.com>
2022-11-15 15:53:13 +0100

    update

--- src/app/book-store/book-list/book-list.component.ts
+++ src/app/book-store/book-list/book-list.component.ts
@@ -17,8 +17,9 @@ export class BookListComponent implements OnInit {
     {name: 'Les 4 accord Toltèques', feedback: 'Life changing book', rating:4}
   ]
 
-  selectedBook: Book | null = null;
-  //bookArray: IBook 
+  selectedBook: IBook | null = null;
+  bookArray: Array<IBook> = new Array<Book>();
+
     onSelect(book: Book): void {
       this.selectedBook = null;
       setTimeout(() => {
@@ -26,6 +27,17 @@ export class BookListComponent implements OnInit {
       }, 10);
     }
   constructor() { }
+
+  updateList(book: IBook) {
+    // Todo: trouver le book par son id et  l'update dans bookArray
+    //this.selectedBook?.name = book.name;
+    this.bookArray.find(element => element.name = book.name);
+  }
+
+  setCurrentBook(book: IBook) {
+    this.selectedBook = null;
+    setTimeout(() => { this.selectedBook = book; }, 10);
+  }
 
   ngOnInit(): void {
 
