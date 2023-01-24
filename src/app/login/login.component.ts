d58f4ee2ca65b24719899b8b71678dec80ca22d2
jessica_norzius <norziusjessica@gmail.com>
2023-01-24 16:03:56 +0100

    loginUpdate

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IUser } from '../../../../shared';
+import { FormBuilder } from '@angular/forms';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  selectedUser: IUser | undefined
+
+  UserForm = this.fb.group({ 
+    username: [''],
+    password: [''] 
+  }, {updateOn: 'submit'});
+  submitted = false;
+
+
+
+  constructor(private fb: FormBuilder, Auth: AuthGuard) { }
 
   ngOnInit(): void {
+    this.submitted = false;
+
+
   }
+  
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.UserForm.valid) {
+      this.selectedUser = {...this.selectedUser!, ...this.UserForm.value!};
+    }
+  }
+  resetForm() {
+    if (this.selectedUser !== null) {
+      this.UserForm.patchValue(this.selectedUser!);
+    }
+  }
+
 
 }
